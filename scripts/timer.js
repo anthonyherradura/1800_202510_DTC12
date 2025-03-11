a834b7fe32f5158b2ae4297c51e2cd326d9c9b93
Anthony Herradura <aherradura@my.bcit.ca>
2025-03-11 15:41:30 -0700

    Add Timer javascript, html, and css

---
+++ scripts/timer.js
@@ -0,0 +1,62 @@
+export default class Timer {
+    constructor(root) {
+        root.innerHTML = Timer.getHTML();
+
+        this.el = {
+            minutes: root.querySelector(".timer__part--minutes"),
+            seconds: root.querySelector(".timer__part--seconds"),
+            control: root.querySelector(".timer__btn--control"),
+            reset: root.querySelector(".timer__btn--reset"),
+        };
+
+        this.interval = null;
+        this.remainingSeconds = 0;
+
+        this.el.control.addEventListener("click", () =>{
+            
+        });
+
+        this.el.reset.addEventListener("click", () =>{
+
+        });
+    }
+
+    updateInterfaceTime(){
+        const minutes = Math.floor(this.remainingSeconds / 60);
+        const seconds = this.remainingSeconds % 60;
+
+        this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+        this.el.seconds.textContent = minutes.toString().padStart(2, "0");
+    }
+
+    updateInterfaceControls() {
+        if (this.interval === null) {
+            this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
+            this.el.control.classList.add("timer__btn--start");
+            this.el.control.classList.remove("timer__btn--stop");
+        }else {
+            this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+            this.el.control.classList.add("timer__btn--stop");
+            this.el.control.classList.remove("timer__btn--start");
+        }
+    }
+
+    start() {
+        if (this.remaining)
+    }
+
+    static getHTML() {
+        return `
+            <h2 id="task_title">Timer:</h2>
+            <span class="timer__part timer__part--minutes">00</span>
+            <span class="timer__part">:</span>
+            <span class="timer__part timer__part--seconds">00</span>
+            <button type="button" class="timer__btn timer__btn--control timer__btn--start">
+                <span class="material-icons">play_arrow</span>
+            </button>
+            <button type="button" class="timer__btn timer__btn--reset">
+                <span class="material-icons">timer</span>
+            </button>
+        `;
+    }
+}
