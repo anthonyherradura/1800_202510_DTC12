a294e9a7fc87834584cdd39bdefe78d9137d0741
Anthony Herradura <aherradura@my.bcit.ca>
2025-04-02 11:05:22 -0700

    Remove calendar and Add timer

--- scripts/timer_app.js
+++ scripts/timer_app.js
@@ -1,14 +1,17 @@
-const start = document.getElementById("start")
-const stop = document.getElementById("stop")
-const reset = document.getElementById("reset")
-const timer = document.getElementById("timer")
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("timer");
+// const inputMinutes = document.getElementById("input_minutes").value;
+const inputMinutesField = document.getElementById("input_minutes"); // Get input field
+const setButton = document.getElementById("set_timer");
 
-let timeLeft = 1500;
+let timeLeft = 0;
 let interval = null;
 
 const updateTimer = () => {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
 
     timer.innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
@@ -22,10 +25,10 @@ const startTimer = () =>{
         timeLeft--;
         updateTimer();
 
-        if(timeLeft === 0){
+        if(timeLeft <= 0){
             clearInterval(interval);
             alert("Time's up! Take a break for 5 minutes or start the timer again :)");
-            timeLeft = 1500;
+            timeLeft = 0;
             updateTimer();
         }
     }, 1000)
@@ -40,11 +43,24 @@ const stopTimer = () => {
 
 const resetTimer = () =>{
         clearInterval(interval);
-        timeLeft = 1500;
+        timeLeft = 0;
         updateTimer();
         interval = null;
 }
 
+const setTimer = (event) => {
+    event.preventDefault(); // Prevent form submission (page refresh)
+    stopTimer();
+    let inputMinutes = parseInt(inputMinutesField.value);
+    if (!isNaN(inputMinutes) && inputMinutes > 0) {
+        timeLeft = inputMinutes * 60;
+    } else {
+        alert("Please enter a valid number of minutes.");
+    }
+    updateTimer();
+};
+
+set_timer.addEventListener("click", setTimer);
 start.addEventListener("click", startTimer);
 stop.addEventListener("click", stopTimer);
 reset.addEventListener("click", resetTimer);
